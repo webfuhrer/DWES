f523ebafe5e5bf5394eeb7827b17c94f8d4f3ab2
Luis Sanz Goizueta <ataraxa@hotmail.com>
2022-09-13 14:15:17 +0200

    Add files via upload

--- colores_body.php
+++ colores_body.php
@@ -1,9 +1,15 @@
 <html>
 <head>
 	<style>
+	<?php
+	$rojo=rand(1, 255);
+	$verde=rand(1, 255);
+	$azul=rand(1,255);
+
+	?>
 	body
 	{
-	background-color: rgb(<?php echo rand(1,255)?>,<?php echo rand(1,255)?>,<?php echo rand(1,255)?>);
+	background-color: rgb(<?php echo $rojo?>, <?php echo $verde?>,<?php echo $azul?>);
 	}
 	</style>
 </head>
