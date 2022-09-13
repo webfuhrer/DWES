ec26e30c6cf251ef4c7df00d2767fdad4e72bf4f
Luis Sanz Goizueta <ataraxa@hotmail.com>
2022-09-13 14:07:02 +0200

    Add files via upload

---
+++ colores_body.php
@@ -0,0 +1,16 @@
+<html>
+<head>
+	<style>
+	body
+	{
+	background-color: rgb(<?php echo rand(1,255)?>,<?php echo rand(1,255)?>,<?php echo rand(1,255)?>);
+	}
+	</style>
+</head>
+
+<body>
+<?php
+echo rand(1, 255);
+?>
+</body>
+</html>
