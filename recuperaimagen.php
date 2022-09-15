35b72f831891f202cdab6182d245d74437ca2044
Luis Sanz Goizueta <ataraxa@hotmail.com>
2022-09-15 11:49:39 +0200

    Add files via upload

---
+++ recuperaimagen.php
@@ -0,0 +1,19 @@
+<html>
+
+<head>
+</head>
+<body>
+<?
+//$ruta_imagen=$_POST["ruta_imagen"];
+echo "<img src=".$_POST["ruta_imagen"].">";
+?>
+<!--
+<img src="<?=$ruta_imagen?>" > 
+-->
+</body>
+</html>
+
+
+
+
+
