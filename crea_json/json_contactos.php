53427e390629f3aeefb4f7afd6d38da12e82ee5c
Luis Sanz Goizueta <ataraxa@hotmail.com>
2022-10-18 13:02:12 +0200

    Creación de JSON con PHP.Recuperado por Javascript

---
+++ crea_json/json_contactos.php
@@ -0,0 +1,27 @@
+<?php
+$archivo_contactos=fopen("agenda.csv", "r");
+$texto_json= "{ \"amigos\":[";
+while(!feof($archivo_contactos))
+{
+    $linea=fgets($archivo_contactos);
+    $arr_datos=explode(",", $linea);
+    if (count($arr_datos)==2)//Para evitar que falle en la línea en blanco
+        {
+            $nombre=$arr_datos[0];
+            $telefono=$arr_datos[1];
+            $texto_json= $texto_json."{";
+            $texto_json=$texto_json."\"nombre\":";
+            $texto_json=$texto_json."\"$nombre\",";
+            $texto_json=$texto_json."\"telefono\":";
+            $texto_json=$texto_json."\"$telefono\"},";
+        }
+
+}
+//Quito la última coma
+//Quiero coger la cadena desde el principio hasta su longitud menos uno
+//PEPE ->4
+//PEP
+$texto_json=substr($texto_json, 0, strlen($texto_json)-1);
+
+$texto_json=$texto_json."]}";
+echo $texto_json;
